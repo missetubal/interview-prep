a0af8d6fddfdebccb318c78efe9d6eea4aab4b90
Michelle Setúbal <missetubal@outlook.com>
2025-04-29 20:17:05 -0300

    ✨ Add home page UI

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,54 @@
+import { Button } from '@/components/ui/button';
+import { InterviewCard } from '@/src/components/InterviewCard/interview-card';
+import { dummyInterviews } from '@/src/constants';
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div>
-      <h1>Page</h1>
-    </div>
+    <>
+      <section className='card-cta'>
+        <div className='flex flex-col gap-6 max-w-lg'>
+          <h2>Get Interview-Ready with AI-Powered practice & Feedback</h2>
+          <p className='text-lg'>
+            Practice on real interview questions & get instant feedback
+          </p>
+          <Button asChild className='btn-primary max-sm:w-full'>
+            <Link href='/interview'>Start an Interview</Link>
+          </Button>
+        </div>
+        <Image
+          height={400}
+          width={400}
+          src='/robot.png'
+          alt='robo'
+          className='max-sm:hidden'
+        />
+      </section>
+      <section className='flex flex-col gap-6 mt-8'>
+        <h2>Your Interviews</h2>
+        <div className='interviews-section'>
+          {dummyInterviews.length > 0 ? (
+            dummyInterviews.map((interview) => (
+              <InterviewCard {...interview} key={interview.id} />
+            ))
+          ) : (
+            <p>You haven't taken any interviews yest</p>
+          )}
+        </div>
+      </section>
+      <section className='flex flex-col gap-6 mt-8'>
+        <h2>Take an Interview</h2>
+        <div className='interviews-section'>
+          {dummyInterviews.length > 0 ? (
+            dummyInterviews.map((interview) => (
+              <InterviewCard {...interview} key={interview.id} />
+            ))
+          ) : (
+            <p>There are no interviews available</p>
+          )}
+        </div>
+      </section>
+    </>
   );
 }
