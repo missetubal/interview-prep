3fdf90986c73c758e7a2dc19b6ffd1687216eb17
Michelle Setúbal <missetubal@outlook.com>
2025-05-03 15:10:03 -0300

    🛂 Update auth actions

--- firebase/client.ts
+++ firebase/client.ts
@@ -1,7 +1,11 @@
-import { getAnalytics } from 'firebase/analytics';
-import { initializeApp, getApp, getApps } from 'firebase/app';
-import { getAuth } from 'firebase-admin/auth';
+// Import the functions you need from the SDKs you need
+import { getApp, getApps, initializeApp } from 'firebase/app';
+// import { getAnalytics } from "firebase/analytics";
+import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
 const firebaseConfig = {
   apiKey: 'AIzaSyArwvauAJAjEr8sQrNjksjiyjzBRxff-Wg',
   authDomain: 'interview-prep-7aa5d.firebaseapp.com',
@@ -13,8 +17,8 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = !getApps.length ? initializeApp(firebaseConfig) : getApp();
-const analytics = getAnalytics(app);
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+// const analytics = getAnalytics(app);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
--- src/components/AuthForm/useAuthForm.ts
+++ src/components/AuthForm/useAuthForm.ts
@@ -1,9 +1,14 @@
 import { useForm } from 'react-hook-form';
-import { authFormSchema, AuthFormType } from './auth-form-schema';
+import { authFormSchema } from './auth-form-schema';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { toast } from 'sonner';
-import { z } from 'zod';
 import { useRouter } from 'next/navigation';
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from 'firebase/auth';
+import { auth } from '@/firebase/client';
+import { signIn, signUp } from '@/lib/actions/auth.action';
 
 export const useAuthForm = (type: FormType) => {
   const router = useRouter();
@@ -16,12 +21,49 @@ export const useAuthForm = (type: FormType) => {
     },
   });
 
-  const onSubmit = form.handleSubmit((values) => {
+  const onSubmit = form.handleSubmit(async (values) => {
     try {
       if (type === 'sign-up') {
+        const { fullname, email, password } = values;
+
+        const userCredentials = await createUserWithEmailAndPassword(
+          auth,
+          email,
+          password
+        );
+
+        const result = await signUp({
+          uid: userCredentials.user.uid,
+          name: name!,
+          email,
+          password,
+        });
+
+        if (!result?.success) {
+          toast.error(result?.message);
+          return;
+        }
+
         toast.success('Account created successfuly. Please sign in.');
         router.push('/sign-in');
       } else {
+        const { email, password } = values;
+
+        const userCredentials = await signInWithEmailAndPassword(
+          auth,
+          email,
+          password
+        );
+
+        const idToken = await userCredentials.user.getIdToken();
+
+        if (!idToken) {
+          toast.error('Sign in failed');
+          return;
+        }
+
+        await signIn({ email, idToken });
+
         toast.success('Sign in successfully');
         router.push('/');
       }
